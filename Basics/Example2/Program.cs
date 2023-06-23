87c22bba41168019fca0e987c5a252d52fd5bba6
NikhilNaik <111115551+NikhilNaik21@users.noreply.github.com>
2023-06-23 16:07:18 +0530

    Add files via upload

---
+++ Basics/Example2/Program.cs
@@ -0,0 +1,34 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+using BasicCal;
+using System.Reflection.Metadata.Ecma335;
+
+internal class program
+{
+    static void Main(String[] args)
+    {
+        Myclass1 obj = new Myclass1();
+        Console.WriteLine("Value of I =" + obj.I);
+        MathExample mathExample = new MathExample();
+        Console.WriteLine("Multiplication = " + mathExample.Multiply());
+    }
+}
+
+public class Myclass1
+{
+    public int I = 10;
+}
+
+namespace BasicCal
+{
+    public class MathExample
+    {
+
+
+        public int Multiply()
+        {
+            return 10 * 10;
+        }
+    }
+}
