d7e4b8990c9ded03db96a793e7d99deb142de70c
NikhilNaik <111115551+NikhilNaik21@users.noreply.github.com>
2023-06-23 16:09:04 +0530

    Add files via upload

---
+++ Basics/DataTypes/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace VariableAndDataType
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Variables and Data Types");
+            int i = 10;
+            double m = 100.20;
+            String str1 = "abde#12$3";
+            bool b1 = true;
+            Console.WriteLine("Implicit Casting");
+             m = Convert.ToDouble(i);
+            Console.WriteLine(m);
+        }
+    }
+}
