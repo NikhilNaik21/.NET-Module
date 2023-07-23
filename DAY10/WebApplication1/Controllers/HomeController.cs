2aa2f4bd700881435b2b907f2f55d85f9ce0291e
NikhilNaik <111115551+NikhilNaik21@users.noreply.github.com>
2023-07-23 21:13:51 +0530

    Add files via upload

---
+++ DAY10/WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+
+        List<Employee> lst = new List<Employee>()
+        {
+            new Employee() { Id=1, Name = "Nikhil" , Address = "Nashik"},
+            new Employee() { Id=2, Name = "Prajwal" , Address = "Satana"},
+            new Employee() { Id=3, Name = "Mohit" , Address = "Jalgaon"},
+        };
+        public ActionResult Index()
+        {
+            return View("ViewHome",lst);
+        }
+    }
+}
