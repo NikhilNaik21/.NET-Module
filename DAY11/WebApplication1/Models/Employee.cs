b797733ea9e4ccf7cb9c867bca3d355ec24e369d
NikhilNaik <111115551+NikhilNaik21@users.noreply.github.com>
2023-07-24 14:03:25 +0530

    Add files via upload

---
+++ DAY11/WebApplication1/Models/Employee.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public class Employee
+    {
+		private int _Id;
+
+		public int Id
+		{
+			get { return _Id; }
+			set { _Id = value; }
+		}
+
+        private string _Name;
+
+        public string Name
+        {
+            get { return _Name; }
+            set { _Name = value; }
+        }
+
+        private string _Address;
+
+        public string Address
+        {
+            get { return _Address; }
+            set { _Address = value; }
+        }
+
+    }
+}
